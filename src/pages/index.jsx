4cb15d2fd3055d9a1152e1176ee6c80089565c38
Kenta11125 <65817751+Kenta11125@users.noreply.github.com>
2023-04-02 14:21:14 +0900

    ⚗️ 不要な記述でいつか消しますが、クリックイベントを書いてみました。

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,10 +5,21 @@ import classes from 'src/styles/Home.module.css';
 import { Footer } from 'src/components/Footer';
 import { Main } from 'src/components/Main';
 import { Header } from 'src/components/Header';
+import { useCallback } from 'react';
 
 const inter = Inter({ subsets: ['latin'] })
 
+ 
+
 export default function Home() {
+
+  const foo = 1;
+  const handleClick = useCallback((e) => {
+    console.log(e.target.href);
+    e.preventDefault();
+    alert(foo);
+  }, []);
+
   return (
      <> {/* <React.Fragment> と同じ意味 import React from 'react を呼び出してから使う*/}
      <div className={classes.container}>
@@ -16,6 +27,12 @@ export default function Home() {
           <title>Index Page</title>
         </Head>
         <Header />
+        <a 
+          href="/about"
+          onClick={handleClick}
+        >
+          ボタン
+        </a>
         <Main page="index"/>
         <Footer name="COLORSDRIP"/>
      </div>

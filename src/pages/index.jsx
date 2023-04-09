9a0957a81b912f61caba63169e294fd2ada5f51c
Kenta11125 <65817751+Kenta11125@users.noreply.github.com>
2023-04-09 15:46:56 +0900

    ✨ useStateで文字列と真偽値の解説

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -14,6 +14,8 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
 
   const [count, setCount] = useState(1);
+  const [text, setText] = useState("");
+  const [isShow, setIsShow] = useState(true);
 
   const handleClick = useCallback(() => {
     if (count < 10) {
@@ -24,6 +26,27 @@ export default function Home() {
     // });
 
   }, [count]);
+
+  const handleChange = useCallback((foo) => {
+    if (foo.target.value.length > 5) {
+      alert('5文字以内にしてください');
+      return;
+    }
+      setText(foo.target.value.trim());
+  }, []);
+
+  const handleDisplay = useCallback(() => {
+                // setIsShow((isShow) => {
+              //   // if(isShow) {
+              //   //   return false;
+              //   // }
+              //   // return true;
+              //   // return isShow ? false : true;
+              //   return !isShow;
+              // });
+              setIsShow((isShow) => !isShow);
+  }, []);
+
   useEffect(() => {
     // console.log(`マウント時： ${count}`);
     document.body.style.backgroundColor = "darkblue";
@@ -33,17 +56,29 @@ export default function Home() {
       document.body.style.backgroundColor = "";
     };
   },[]);
+
+
+
   return (
      <> {/* <React.Fragment> と同じ意味 import React from 'react を呼び出してから使う*/}
+     {/* JSXはreturn文の中でifが使えないので三項演算子で記述する必要がある */}
      <div className={classes.container}>
         <Head>
           <title>Index Page</title>
         </Head>
         <Header />
-        <h1>{count}</h1>
+        {isShow ? <h1>{count}</h1> : null}
           <button onClick={handleClick}>
             ボタン
           </button>
+          <br></br>
+          <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
+          <br></br>
+          <input 
+          type="text" 
+          value={text} 
+          onChange={handleChange}
+          />
         <Main page="index"/>
         <Footer name="COLORSDRIP"/>
      </div>

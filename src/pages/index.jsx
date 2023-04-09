c28447fb934dd74daa3390d472c4feb7c83bced5
Kenta11125 <65817751+Kenta11125@users.noreply.github.com>
2023-04-09 14:35:52 +0900

    ⚡️ useEffectとuseCallbackの説明

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,7 +5,7 @@ import classes from 'src/styles/Home.module.css';
 import { Footer } from 'src/components/Footer';
 import { Main } from 'src/components/Main';
 import { Header } from 'src/components/Header';
-import { useEffect,useState } from 'react';
+import { useEffect,useState,useCallback } from 'react';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,18 +15,21 @@ export default function Home() {
 
   const [count, setCount] = useState(1);
 
-  const handleClick = (e) => {
-    setCount((fooo) => fooo + 1);//これでも一緒　アロー関数
-    setCount((fooo) => fooo + 1);//これでも一緒　アロー関数
+  const handleClick = useCallback(() => {
+    if (count < 10) {
+      setCount((foo) => foo + 1);//これでも一緒　アロー関数
+    }
     // setFoo(function(foo) {
     //   return foo + 1;
     // });
 
-  };
+  }, [count]);
   useEffect(() => {
+    // console.log(`マウント時： ${count}`);
     document.body.style.backgroundColor = "darkblue";
 
     return () => {
+      // console.log(`アンマウント時： ${count}`); //バックスラッシュ
       document.body.style.backgroundColor = "";
     };
   },[]);

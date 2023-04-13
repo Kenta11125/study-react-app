0e3b62b0deaf188a562c454f7bbaaaeb4f117454
Kenta11125 <65817751+Kenta11125@users.noreply.github.com>
2023-04-14 01:02:21 +0900

    ✨ コンポーネント間のStateのリフトアップ

--- src/components/Links/index.jsx
+++ src/components/Links/index.jsx
@@ -2,36 +2,20 @@
 import { Inter } from 'next/font/google'
 import classes from 'src/components/Links/Links.module.css'
 
+
 const inter = Inter({ subsets: ['latin'] })
 
-const ITMES = [
-  { href: "https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
-    title: "Docs",
-    discription: "Find in-depth information about Next.js features and&nbsp;API.",
-  },
-  { href: "https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
-  title: "Learn",
-  discription: "Learn about Next.js in an interactive course with&nbsp;quizzes!",
-  },
-  { href: "https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
-  title: "Templates",
-  discription: "Discover and deploy boilerplate example Next.js&nbsp;projects.",
-  },
-  { href: "https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
-  title: "Deploy",
-  discription: "Instantly deploy your Next.js site to a shareable URL with&nbsp;Vercel.",
-  },
-  // { href: "https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
-  // title: "Deploy <span>-&gt;</span>",
-  // discription: "Instantly deploy your Next.js site to a shareable URL with&nbsp;Vercel.",
-  // },
-];
 
-export function Links() {
+
+export function Links({ items, handleReduce }) {
+
   return (
     <>
         <div className={classes.grid}>
-          {ITMES.map((item) => {
+          <button onClick ={handleReduce} >消す</button>
+          <br></br>
+          <br></br>
+          {items.map((item) => {
             return(
               <a
                 key={item.href}
--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -5,16 +5,40 @@ import classes from 'src/components/Main/Main.module.css';
 import { Links } from 'src/components/Links/';
 import { Logo } from 'src/components/MainLogo';
 import { Headline } from 'src/components/Headline';
-
+import React, { useState, useCallback } from 'react';
 
 const inter = Inter({ subsets: ['latin'] })
 
+const ITEMS = [
+  { href: "https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
+    title: "Docs",
+    discription: "Find in-depth information about Next.js features and&nbsp;API.",
+  },
+  { href: "https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
+  title: "Learn",
+  discription: "Learn about Next.js in an interactive course with&nbsp;quizzes!",
+  },
+  { href: "https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
+  title: "Templates",
+  discription: "Discover and deploy boilerplate example Next.js&nbsp;projects.",
+  },
+  { href: "https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
+  title: "Deploy",
+  discription: "Instantly deploy your Next.js site to a shareable URL with&nbsp;Vercel.",
+  },
+  // { href: "https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
+  // title: "Deploy <span>-&gt;</span>",
+  // discription: "Instantly deploy your Next.js site to a shareable URL with&nbsp;Vercel.",
+  // },
+];
+
 export function Main(props) {
-
-
-
-
-
+  const [items,setItems] = useState(ITEMS);
+  const handleReduce = useCallback(() => {
+    setItems((prevItem) => {
+        return prevItem.slice(0,prevItem.length - 1);
+      });
+  }, []);
   return (
      <> {/* <React.Fragment> と同じ意味 import React from 'react を呼び出してから使う*/}
       <main className={classes.main}>
@@ -43,7 +67,7 @@ export function Main(props) {
         </div>
         </div>
         <Logo />
-        <Links />
+        <Links items={items} handleReduce={handleReduce} />
       </main>
     </>
   )
